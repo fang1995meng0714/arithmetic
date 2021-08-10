709677670a3a3e02d5ca84560ad3131c517a0472
fangyuemeng <yuemeng.fang@macrowing.com>
2021-08-10 10:56:37 +0800

    增加平衡树结构

--- 10tree/BinarySearchTree.js
+++ 10tree/BinarySearchTree.js
@@ -29,6 +29,125 @@ import {Compare, defaultCompare} from "./../util.js";
             }
         }
     }
+
+    //中序遍历
+    inOrderTraverse(callback) {
+        this.inOrderTraverseNode(this.root, callback);
+    }
+
+    inOrderTraverseNode(node, callback) {
+        if(node != null) {
+            this.inOrderTraverseNode(node.left, callback);
+            callback(node.key);
+            this.inOrderTraverseNode(node.right, callback)
+        } 
+    }
+
+    // 先序遍历
+    preOrderTraverse(callback) {
+        this.preOrderTraverseNode(this.root, callback);
+    }
+
+    preOrderTraverseNode(node, callback) {
+        if(node != null) {
+            callback(node.key);
+            this.preOrderTraverseNode(node.left, callback);
+            this.preOrderTraverseNode(node.right, callback);
+        }
+    }
+
+    //后序遍历
+    postOrderTraverse(callback) {
+        this.postOrderTraverseNode(this.root, callback);
+    }
+
+    postOrderTraverseNode(node, callback) {
+        if(node != null) {
+            this.postOrderTraverseNode(node.left, callback);
+            this.postOrderTraverseNode(node.right, callback);
+            callback(node.key);
+        }
+    }
+
+    //最小值
+    min() {
+        return this.minNode(this.root);
+    }
+
+    minNode(node) {
+        let current = node;
+        while(current != null && current.left != null) {
+            current = current.left;
+        }
+        return current;
+    }
+
+    // 最大值
+    max() {
+        return this.maxNode(this.root);
+    }
+
+    maxNode(node) {
+        let current = node;
+        while(current != null && current.right != null) {
+            current = current.right;
+        }
+
+        return current;
+    }
+
+    search(key) {
+        return this.searchNode(this.root, key);
+    }
+
+    searchNode(node, key) {
+        if(node == null) {
+            return false;
+        }
+
+        if(this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            return this.searchNode(node.left, key);
+        } else if(this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            return this.searchNode(node.right, key);
+        } else {
+            return true;
+        }
+    }
+
+    remove(key) {
+        this.root = this.removeNode(this.root, key);
+    }
+
+    removeNode(node, key) {
+        if(node == null) {
+            return null;
+        }
+
+        if(this.compareFn(key, node.key) === Compare.LESS_THAN) {
+            node.left = this.removeNode(node.left, key);
+            return node;
+        } else if(this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+            node.right = this.removeNode(node.right, key);
+            return node;
+        } else {
+            if(node.left == null && node.right == null) {
+                node = null;
+                return node;
+            }
+            if(node.left == null) {
+                node = node.right;
+                return node;
+            } else if(node.right == null) {
+                node = node.left;
+                return node;
+            }
+            const aux = this.minNode(node.right);
+            node.key = aux.key;
+            node.right = this.removeNode(node.right, aux.key);
+            return node;
+        }
+    }
+
  }
 
  const tree = new BinarySearchTree();
@@ -46,5 +165,14 @@ import {Compare, defaultCompare} from "./../util.js";
  tree.insert(20); 
  tree.insert(18); 
  tree.insert(25);
- console.log(tree);
  tree.insert(6)
+//  console.log(tree);
+
+// const printNode = (value) => console.log(value); // {6} 
+// tree.inOrderTraverse(printNode); // {7}
+// tree.preOrderTraverse(printNode)
+// tree.postOrderTraverse(printNode);
+// console.log(tree.search(100));
+tree.remove(15)
+// console.log(tree)
+
